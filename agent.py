635f85700c99029209280f15ab2f088f1539e519
Morten Kjelsrud <mortkjels@gmail.com>
2026-08-04 17:16:44 +0200

    changes in python files

--- agent.py
+++ agent.py
@@ -3,32 +3,25 @@ from google import genai
 from google.genai import types
 from pathlib import Path
 from dotenv import load_dotenv
-import fantasy
 import json
+from datetime import datetime
 
 BASE_DIR = Path(__file__).parent
+DATA_DIR = BASE_DIR / "data"
 load_dotenv(BASE_DIR / ".env")
 API_AGENT = os.getenv("API_KEY_AGENT")
 
 client = genai.Client(api_key=API_AGENT)
 
 
-def filter_data_before_prompt():
-    with open (fantasy.DATA_DIR / "injuries.json", "r") as file:
-        z = json.loads(file.read())
+# def filter_data_before_prompt():
 
-        # print(z['response'][0])
-        for key, value in z.items():
-            for item in value:
-                print(item)
+# filter_data_before_prompt()
 
 
 def analyzing_with_model():
-    
     response = client.models.generate_content(
         model="gemma-4-31b-it", 
         contents=["", "Hvilke skader skjedde i runde 1 av sesongen i PL? Oppsummer spiller, skade og hvilket lag."] )
 
     return response.text
-
-filter_data_before_prompt()
--- fantasy.py
+++ fantasy.py
@@ -11,43 +11,65 @@ load_dotenv(BASE_DIR / ".env")
 API_FANTASY = os.getenv("API_KEY_FANTASY")
 
 
-def get_fixtures(params):
-    url = "https://v3.football.api-sports.io/fixtures"
-    headers = {"x-apisports-key": API_FANTASY}
-    response = requests.get(url, 
-                        headers=headers,
-                        params = params)
-    formatted_data = json.dumps(response.json(), indent=4)
-    with open(DATA_DIR / "fixtures.json", "w", encoding="utf-8") as file:
-        file.write(formatted_data)
-    return formatted_data
+# def get_fixtures(params):
+#     url = "https://v3.football.api-sports.io/fixtures"
+#     headers = {"x-apisports-key": API_FANTASY}
+#     response = requests.get(url, 
+#                         headers=headers,
+#                         params = params)
+#     formatted_data = json.dumps(response.json(), indent=4)
+#     with open(DATA_DIR / "fixtures.json", "w", encoding="utf-8") as file:
+#         file.write(formatted_data)
+#     return formatted_data
 
-def get_league_standings(params):
-    url = "https://v3.football.api-sports.io/standings"
-    headers = {"x-apisports-key": API_FANTASY}
-    response = requests.get(url, 
-                        headers=headers,
-                        params = params)
-    formatted_data = json.dumps(response.json(), indent=4)
-    with open(DATA_DIR / "standings.json", "w", encoding="utf-8") as file:
-        file.write(formatted_data)
-    return formatted_data
+# def get_league_standings(params):
+#     url = "https://v3.football.api-sports.io/standings"
+#     headers = {"x-apisports-key": API_FANTASY}
+#     response = requests.get(url, 
+#                         headers=headers,
+#                         params = params)
+#     formatted_data = json.dumps(response.json(), indent=4)
+#     with open(DATA_DIR / "standings.json", "w", encoding="utf-8") as file:
+#         file.write(formatted_data)
+#     return formatted_data
 
-def get_injuries(params):
-    url = "https://v3.football.api-sports.io/injuries"
-    headers = {"x-apisports-key": API_FANTASY}
-    response = requests.get(url, 
-                        headers=headers,
-                        params = params)
-    formatted_data = json.dumps(response.json(), indent=4)
-    with open(DATA_DIR / "injuries.json", "w", encoding="utf-8") as file:
-        file.write(formatted_data)
-    return formatted_data
+# def get_injuries(params):
+#     url = "https://v3.football.api-sports.io/injuries"
+#     headers = {"x-apisports-key": API_FANTASY}
+#     response = requests.get(url, 
+#                         headers=headers,
+#                         params = params)
+#     formatted_data = json.dumps(response.json(), indent=4)
+#     with open(DATA_DIR / "injuries.json", "w", encoding="utf-8") as file:
+#         file.write(formatted_data)
+#     return formatted_data
 
 
-get_injuries(params={"season": 2022, "league": 39})
-get_league_standings(params={"season": 2022, "league": 39})
-get_fixtures(params={"season": 2022, "league": 39})
+def find_gameweek():
+    with open (DATA_DIR / "fixtures.json", "r") as file:
+        x = json.loads(file.read())
+        gameweeks = x["response"]
+        gameweek = {}
+        for rounds in gameweeks:
+            round = rounds["league"]["round"]
+
+# def find_injuries():
+#     find_gameweek()
+#     with open (DATA_DIR / "injuries.json", "r") as file:
+#         z = json.loads(file.read())
+#         info = z["response"]
+#         injuries = []
+#         for items in info:
+#             injury_date = items["fixture"]["date"]
+#             if injury_date >= startdate and injury_date <= enddate:
+#                 injuries.append(items)
+#             else:
+#                 break
+#         print(injuries[0])
+
+# get_injuries(params={"season": 2022, "league": 39})
+# get_league_standings(params={"season": 2022, "league": 39})
+# get_fixtures(params={"season": 2022, "league": 39})
 
 
 #Seasons available: 2022, 2023, 2024
