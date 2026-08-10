b119be63e9db89e9214a1859e9aaa0466ccda2e6
Morten Kjelsrud <mortkjels@gmail.com>
2026-08-10 20:04:07 +0200

    Reasonable model as for now

--- agentic.py
+++ agentic.py
@@ -18,13 +18,12 @@ def question_to_parse():
     query = client.models.generate_content(
         model="gemma-4-31b-it", 
         contents=[f'{question}', 'Uavhengig av hvordan spørsmålet er formulert, '
-        'så skal du hente ut infoen som sesong, runde og request (skader, overganger etc), som et '
+        'så skal du hente ut infoen som sesong, runde og request (skader (ute, utilgjengelig, injuries, skadet), overganger (kjøpt, salg, transfers, signeringer) etc), som et '
         'json-format. Jeg skal kun ha output fra deg som dette'
         '{"season": x, "gameweek": y, "request": z.}. '
-        'Request kan komme på norsk, men returner i formatet på engelsk'] )
+        'Request kan komme på norsk, men returner i formatet på engelsk. Så injuries, transfers, fixtures, standings etc.'] )
     output = query.text
     plain_text = output.split("\n")
-    print(plain_text)
     plain_text_to_json = json.loads(plain_text[1])
     return plain_text_to_json
 
@@ -42,4 +41,14 @@ def analyzing_with_model():
     return response.text
 
 print(analyzing_with_model())
+
+
+# Ikke godkjent
+# Hvilke spillere var ute i åpningsrunden 2023?
+
+# Godkjent 
+# Skader i runde 1 i 2022
+# Hvem var skadet i første runde i 2022?
+# Skader PL GW 1 2022
+# Skader fra gameweek 20 i 2024
 
