455d6ee92b44ca0acf0d619290e1b1f4af2c9d53
Morten Kjelsrud <mortkjels@gmail.com>
2026-09-25 19:24:19 +0200

    Updates

--- agentic.py
+++ agentic.py
@@ -21,7 +21,8 @@ def question_to_parse():
         'så skal du hente ut infoen som sesong, runde og request (skader (ute, utilgjengelig, injuries, skadet), overganger (kjøpt, salg, transfers, signeringer) etc), som et '
         'json-format. Jeg skal kun ha output fra deg som dette'
         '{"season": x, "gameweek": y, "request": z.}. '
-        'Request kan komme på norsk, men returner i formatet på engelsk. Så injuries, transfers, fixtures, standings etc.'] )
+        'Request kan komme på norsk, men returner i formatet på engelsk. Så injuries, transfers, fixtures, standings etc.'
+        'Det er ekstremt viktig at du tar hensyn til request. Det viktigste er det jeg ønsker fra den visse sesongen og runden, typisk skader, overganger, fixtures osv'] )
     output = query.text
     plain_text = output.split("\n")
     plain_text_to_json = json.loads(plain_text[1])
@@ -35,9 +36,11 @@ matches = fantasy.matches_that_gameweek(filename, gameweek)
 
 def analyzing_with_model():
     response = client.models.generate_content(
-        model="gemma-4-31b-it", 
+        model="gemini-3.1-flash-lite", 
         contents=[f'{matches}','Oppsummer dette til et leselig og pent format slik at jeg kan ha kontroll over byttene mine for neste runde i FPL. Jeg ønsker at du tar for deg alle lag.'] )
 
     return response.text
 
 print(analyzing_with_model())
+
+
